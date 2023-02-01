546218237443a6b841767fcef47ba816151564d9
Андрейчук Максим <maximandreychuk1999gmail.com>
2023-02-01 16:31:11 +0300

    2 review

--- exceptions.py
+++ exceptions.py
@@ -1,3 +1,19 @@
-class MyException(Exception):
-    """Собственное исключение."""
+class NotTokensEx(Exception):
+    """Токены не обнаружены."""
     pass
+
+class NoSendMessageEx(Exception):
+    """Cообщение не отправлено."""
+    pass 
+
+class InvalidHttpCodeEx(Exception):
+    """Статус HTTP-кода равен не 200."""
+    pass
+
+class InvalidRequestEx(Exception):
+    """Ошибка получения request."""
+    pass
+
+class UnknownStatusEx(Exception):
+    """Hеизвестный статус домашней работы."""
+    pass 
--- homework.py
+++ homework.py
@@ -2,15 +2,19 @@ import http
 import logging
 import os
 import requests
+import sys
 import telegram
 import time
 
 from dotenv import load_dotenv
-from exceptions import MyException
+from exceptions import (NotTokensEx, NoSendMessageEx,
+                       InvalidHttpCodeEx, InvalidRequestEx,
+                       UnknownStatusEx)
 
+load_dotenv()
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = 549065222
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -23,8 +27,6 @@ HOMEWORK_VERDICTS = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-load_dotenv()
-
 logging.basicConfig(
     level=logging.INFO,
     filename='logging_for_homework.log',
@@ -35,33 +37,35 @@ logger = logging.getLogger(__name__)
 
 def check_tokens():
     """Проверка токенов. Если их нет - программа останавливается."""
-    if PRACTICUM_TOKEN is None or TELEGRAM_TOKEN is None:
+    if all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]) is False:
         logger.critical('Где токены?')
-        raise MyException('Где токены?')
+        sys.exit()
 
 
 def send_message(bot, message):
     """С помощью этой функции отправляем сообщение в чат."""
     try:
+        logger.debug('Отправка сообщения..')
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
         logger.debug('Успешная отправка сообщения!')
     except Exception:
         logger.error('Не получается отправить сообщение')
-        raise MyException('Не получается отправить сообщение')
+        raise NoSendMessageEx('Не получается отправить сообщение')
 
 
 def get_api_answer(timestamp):
     """Получаем результат запроса к API."""
     try:
         PAYLOAD = {'from_date': timestamp}
+        logger.debug('Отправка запроса к API..')
         response = requests.get(ENDPOINT, headers=HEADERS, params=PAYLOAD)
         if response.status_code != http.HTTPStatus.OK:
             logger.error(f'Ошибка. Код запроса = {response.status_code}')
-            raise MyException(f'Ошибка. Код запроса = {response.status_code}')
+            raise InvalidHttpCodeEx(f'Ошибка. Код запроса = {response.status_code}')
         return response.json()
     except Exception as error:
         logger.error(f'Ошибка получения request: {error}')
-        raise MyException(f'Ошибка получения request: {error}')
+        raise InvalidRequestEx(f'Ошибка получения request: {error}')
 
 
 def parse_status(homework):
@@ -72,30 +76,30 @@ def parse_status(homework):
         if status in HOMEWORK_VERDICTS:
             verdict = HOMEWORK_VERDICTS.get(status)
             return ('Изменился статус проверки работы'
-                    + f' "{homework_name}". {verdict}')
-        logger.error('Неожиданный статус домашней работы: '
-                     + f'{verdict}, обнаруженный в ответе API')
-        raise MyException('Hеизвестный статус')
+                    f' "{homework_name}". {verdict}')
+        logger.error('Неожиданный статус домашней работы:'
+                     f' {verdict}, обнаруженный в ответе API')
+        raise UnknownStatusEx('Hеизвестный статус')
     logger.error('Нет ключей')
     raise KeyError('Нет ключей')
 
 
 def check_response(response):
-    """Просто проверяем наш запрос на предмет различных ошибок."""
+    """Проверяем наш запрос на предмет различных ошибок."""
     try:
-        if type(response) == dict:
-            if 'homeworks' in response.keys():
-                if type(response['homeworks']) == list:
-                    if response['homeworks'] is not EMPTY_LIST:
-                        return response['homeworks']
-                    logger.error('Список пуст')
-                    raise TypeError('Список пуст')
-                logger.error('Значение ключа homeworks не list')
-                raise TypeError('Значение ключа homeworks не list')
+        if not isinstance(response, dict):
+            logger.error('Запрос к API вернул не словарь')
+            raise TypeError('Запрос к API вернул не словарь')
+        if 'homeworks' not in response:
             logger.error('Нет ключа homeworks')
             raise KeyError('Нет ключа homeworks')
-        logger.error('Запрос к API вернул не словарь')
-        raise TypeError('Запрос к API вернул не словарь')
+        if not isinstance(response['homeworks'], list):
+            logger.error('Значение ключа homeworks не list')
+            raise TypeError('Значение ключа homeworks не list')
+        if response['homeworks'] is EMPTY_LIST:
+            logger.error('Список пуст')
+            raise TypeError('Список пуст')
+        return response['homeworks']
     except Exception as error:
         logger.error(error)
         raise TypeError(error)
@@ -122,9 +126,9 @@ def main():
             bot.send_message(TELEGRAM_CHAT_ID, message)
 
         finally:
-            timestamp = int(time.time())
             time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
-    main()
+   main()
+
