8ef8e2a041a629404674fc8f1554c235159605a3
Андрейчук Максим <maximandreychuk1999gmail.com>
2023-02-03 23:20:14 +0300

    2 review

--- fileex.py
+++
@@ -1,4 +0,0 @@
-f = open('xyz.txt','w')  # открытие в режиме записи
-f.write('Hello \n World')  # запись Hello World в файл
-
-f.close()  # закрытие файла
--- yhome.py
+++ yhome.py
@@ -2,11 +2,21 @@ import requests
 from bs4 import BeautifulSoup 
 import pandas 
 
-URL_TEMPLATE = "https://www.pressfoto.ru/galleries/trends-2017"
-r = requests.get(URL_TEMPLATE)
-try:
-    f = open('example_1.txt', 'w')
-    f.write(r.text)  
-    f.close()  
-except Exception as error:
-    print(error)
+IMAGE_COUNTER = 0
+URL_TEMPLATE = 'https://www.vovatit.com/artworks'
+response = requests.get(URL_TEMPLATE).text
+
+soup = BeautifulSoup(response, 'lxml')
+block = soup.find('div', class_ = 'w-dyn-list')
+block_image = block.find_all('div', class_ = 'collection-item w-dyn-item w-col w-col-4')
+
+for image in block_image:
+    link = image.find('img').get('src')
+    link_photo = requests.get(link).content
+
+    with open(f'images/{IMAGE_COUNTER}.jpg', 'wb') as file:
+        file.write(link_photo)
+
+    IMAGE_COUNTER += 1
+    print(f'{IMAGE_COUNTER} скачано')
+
